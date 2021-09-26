032533a7762db2b1b73a96e4148a428aaa64eaa2
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2021-09-26 06:49:21 +0200

    Add config.py with common constants

---
+++ wcbstudent/config.py
@@ -0,0 +1,14 @@
+"""
+Common config for Wocabee Student
+"""
+
+PATH_VOCABULARY = "resources/vocabulary.csv"
+
+PATH_BROWSER = "resources/msedgedriver.exe"
+
+URL_APP = "https://www.wocabee.app/app/"
+
+NUM_ANSWERS = 100
+
+VAR_USER = "WCB_USERNAME"
+VAR_PASS = "WCB_PASSWORD"
