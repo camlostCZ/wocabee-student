e717a85de4566aa0819b7721dea8ef8220851a7b
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2021-09-30 21:18:58 +0200

    Add basic exception handling

--- wcbstudent/__main__.py
+++ wcbstudent/__main__.py
@@ -1,5 +1,14 @@
+import sys
+
+from selenium.common.exceptions import SessionNotCreatedException
 from student import Student
 
-app = Student()
-app.run()
-
+try:
+    app = Student()
+    app.run()
+except FileNotFoundError:
+    print("Error: Vocabulary not found.", file=sys.stderr)
+except SessionNotCreatedException:
+    print("Error: Web browser driver not found. Download correct version:\n"
+        "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/",
+        file=sys.stderr)
